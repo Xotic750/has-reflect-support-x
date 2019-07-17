89fbef0a17f6a91b034ec998cc958d914e2512df
Graham Fairweather <xotic750@gmail.com>
2019-07-17 20:09:16 +0200

    :bookmark: v2.0.8
    Auto build

--- dist/has-reflect-support-x.js
+++ dist/has-reflect-support-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T13:22:57.048Z",
+  "date": "2019-07-17T18:09:11.907Z",
   "describe": "",
   "description": "Tests if ES6 Reflect is supported.",
   "file": "has-reflect-support-x.js",
   "hash": "ad807a8c6059da73f442",
   "license": "MIT",
-  "version": "2.0.7"
+  "version": "2.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
