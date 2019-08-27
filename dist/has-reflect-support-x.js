38b2e061c0c01ec7fac5ed92b307b41fb996e196
Graham Fairweather <xotic750@gmail.com>
2019-08-27 21:30:16 +0200

    Maintenance

--- dist/has-reflect-support-x.js
+++ dist/has-reflect-support-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-20T12:38:19.064Z",
+  "date": "2019-08-27T19:30:10.203Z",
   "describe": "",
   "description": "Tests if ES6 Reflect is supported.",
   "file": "has-reflect-support-x.js",
